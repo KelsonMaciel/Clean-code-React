65f7ee9a0f220eafa77cafa98f8481a96b6329a5
kelsonjunior <65233486+kelsonjunior@users.noreply.github.com>
2020-10-31 02:52:55 -0300

    test: ensure AxiosHttpClient calls axios correct body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,6 +2,6 @@ import { HttpPostParams } from '@/data/protocols/http'
 import Axios from 'axios'
 export class AxiosHttpClient {
   async post (params: HttpPostParams<any>): Promise<void> {
-    await Axios.post(params.url)
+    await Axios.post(params.url, params.body)
   }
 }
