e0f94fac375ccbc46ee6e88c1d56de4a96d84b8e
kelsonjunior <65233486+kelsonjunior@users.noreply.github.com>
2020-11-08 19:47:38 -0300

    feat: ensure Login calls validation with correct emails

--- src/presentation/component/form-status/form-status.tsx
+++ src/presentation/component/form-status/form-status.tsx
@@ -4,11 +4,11 @@ import Styles from './form-status-styles.scss'
 import Context from '@/presentation/component/context/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { state, errorState } = useContext(Context)
+  const { state } = useContext(Context)
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
       {state.isLoading && <Spinner className={Styles.spinner} />}
-      {errorState.main && <span className={Styles.error}>{errorState.main}</span>}
+      {state.main && <span className={Styles.error}>{state.main}</span>}
     </div>
   )
 }
