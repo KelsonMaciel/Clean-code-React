863b51db0687781b9875272f3b7b8586e8f52a7a
kelsonjunior <65233486+kelsonjunior@users.noreply.github.com>
2020-11-08 16:43:54 -0300

    feat: ensure Login start with input with required state

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,20 +3,19 @@ import Styles from './login-styles.scss'
 import { Header, Footer, Input, FormStatus } from '@/presentation/component'
 import Context from '@/presentation/component/context/form/form-context'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
+  const [state] = useState({
+    isLoading: false
+  })
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
 
   return <div className={Styles.login}>
     <Header />
-    <Context.Provider value={state}>
+    <Context.Provider value={{ state, errorState }}>
       <form className={Styles.form}>
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="digite seu email"/>
